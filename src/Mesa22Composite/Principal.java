280081fbbc5372f61cbe590ddb08cfbe1195cf38
GasparPSousa <gasparufrj@gmail.com>
2021-09-27 10:28:16 -0300

    Inserindo comentarios na classe principal

--- src/Mesa22Composite/Principal.java
+++ src/Mesa22Composite/Principal.java
@@ -6,12 +6,13 @@ public class Principal {
         // instanciar as figuras
         Retangulo retangulo01 = new Retangulo(5, 4);
         Circulo circulo01 = new Circulo(1);
-
         Retangulo retangulo02 = new Retangulo(6, 4);
         Triangulo triangulo = new Triangulo(2, 2);
 
+        // Instanciando os combos vagao e a locomotiva
         Combo vagao = new Combo();
         Combo locomotiva = new Combo();
+
 
         // Adicionando as figuras no Combo Vagão
         vagao.addArea(retangulo01);
@@ -19,8 +20,9 @@ public class Principal {
         vagao.addArea(circulo01);
         vagao.addArea(circulo01);
 
-        //
+        // Calculando a área do vagao
         System.out.println(vagao.calcularArea());
+
 
         // Adicionando as figuras do Combo Locomotiva
         locomotiva.addArea(retangulo02);
@@ -28,15 +30,11 @@ public class Principal {
         locomotiva.addArea(circulo01);
         locomotiva.addArea(triangulo);
 
-        //
+        // Calculando a área do Combo Locomotiva
         System.out.println(locomotiva.calcularArea());
 
+        // Calculando a área total do Trem
         System.out.println("A área total do Trem é : " + (vagao.calcularArea() + locomotiva.calcularArea()));
-
-
-
-
-
 
     }
 }
